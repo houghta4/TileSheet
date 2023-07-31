650b328d9d039566d1c100d6dc9e3f691d24f47f
Vince <houghta4@uwm.edu>
2023-07-30 20:37:46 -0500

    working with specific tile

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ let formData = {
     spriteSizeX: "0",
     spriteSizeY: "0",
     totalRows: "0",
-    totalCols: "0"
+    totalCols: "0",
 };
+
+let bgImg = "";
 
 // Need to check everything is filled out
 const onFormSubmit = (e) => {
@@ -14,7 +16,6 @@ const onFormSubmit = (e) => {
     formData = Object.fromEntries(data.entries());
     formData.totalRows = (+formData.sheetSizeX / +formData.spriteSizeX).toString();
     formData.totalCols = (+formData.sheetSizeY / +formData.spriteSizeY).toString();
-    console.log(formData);
     document.querySelector('#content').style.width = `${formData.sheetSizeX}px`;
     document.querySelector('#content').style.height = `${formData.sheetSizeY}px`;
 
@@ -30,31 +31,49 @@ const onFileSelect = (e) => {
 
     reader.onload = readerEvent => {
         const content = readerEvent.target.result;
-        document.querySelector('#content').style.backgroundImage = 'url(' + content + ')';
+        bgImg = content;
+        document.querySelector('#content').style.backgroundImage = 'url(' + bgImg + ')';
     };
 }
 
 const onClickTile = (e) => {
     e.preventDefault();
+    console.log(formData);
     let results = document.getElementById("results");
+    let entry = document.createElement("div");
+    let tileWrapper =document.createElement("div");
     let tile = document.createElement("div");
-    let row = Math.floor(e.pageY / +formData.spriteSizeY);
-    let col = Math.floor(e.pageX / +formData.spriteSizeX);
-    let idx = row * +formData.totalRows + col;
-    tile.innerHTML = `(row, col): ((${row}, ${col}) idx: ${idx}`;
-    results.appendChild(tile);
+    let tileDetails = document.createElement("div");
+    const row = Math.floor(e.pageY / +formData.spriteSizeY);
+    const col = Math.floor(e.pageX / +formData.spriteSizeX);
+    const idx = row * +formData.totalRows + col;
+
+    tileWrapper.classList.add("checker-bg");
+
+    tileDetails.innerHTML = `(row, col): ((${row}, ${col}) idx: ${idx}`;
+
+    tile.style.backgroundImage = 'url(' + bgImg + ')';
+    tile.style.backgroundColor = 'red';
+    tile.style.background = `no-repeat -${col * +formData.spriteSizeX}px -${row * +formData.spriteSizeY}px url(${bgImg})`
+    tile.style.width = `${formData.spriteSizeX}px`;
+    tile.style.height = `${formData.spriteSizeY}px`;
+    tile.style.overflow = 'hidden';
+
+    entry.classList.add("flex-wrapper");
+
+    tileWrapper.appendChild(tile);
+    entry.appendChild(tileWrapper);
+    entry.appendChild(tileDetails);
+    results.appendChild(entry);
 }
 
 const onMouseOverTile = (e) => {
     e.preventDefault();
-    if (!formData)
-        return;
     let content = document.getElementById("content");
-    let row = Math.floor(e.pageY / +formData.spriteSizeY);
-    let col = Math.floor(e.pageX / +formData.spriteSizeX);
-    let idx = row * +formData.totalRows + col;
+    const row = Math.floor(e.pageY / +formData.spriteSizeY);
+    const col = Math.floor(e.pageX / +formData.spriteSizeX);
+    const idx = row * +formData.totalRows + col;
     content.title = `idx: ${idx}`;
-
 }
 
 window.onload = function (_e) {
